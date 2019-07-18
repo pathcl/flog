ae6ca5e7b2ec17dff7e6ba03cce0732a9e36c76c
Luis San Martin <luis@sanmartin.io>
2019-07-18 22:21:34 +0200

    for loop

--- main.go
+++ main.go
@@ -10,9 +10,7 @@ import (
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	opts := ParseOptions()
-        for {
 	if err := Run(opts); err != nil {
 		cfmt.Warningln(err.Error())
-	}
-    }
+        }
 }
